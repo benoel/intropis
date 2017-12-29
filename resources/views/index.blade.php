d8054830fe5f96a4a9282ec3e98f944c95eecda1
Ibnu Abdul Azis <ben_oel@yahoo.com>
2017-12-29 23:26:21 +0700

    setting for first awesome look

---
+++ resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Intropis</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+    body {
+        font-family: 'Raleway', sans-serif;
+        margin: 0;
+    }
+    h1{
+        font-size: 80px;
+        color: #fff;
+        padding: 0;
+        margin: 0 auto;
+        display: block;
+    }
+    p{
+        display: block;
+        color: #fff;
+        letter-spacing: 3px;
+        margin: 20px auto;
+        text-align: center;
+    }
+    .center{
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        padding: 0;
+        margin: 0;
+        height: 100vh;
+        background-color: #263238;
+    }
+</style>
+</head>
+<body>
+   <div class="center">
+    <div class="content">
+        <h1>Intropis</h1>
+        <p>By Ibnu Adbul Azis</p>
+    </div>
+</div>
+</body>
+</html>
