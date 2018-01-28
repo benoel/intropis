0c0996bdd3f8e5627bb2a48132aeed7fb87a1bce
Ibnu Abdul Azis <ben_oel@yahoo.com>
2018-01-29 02:09:24 +0700

    add granim js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+    <script src="{{ asset('vendor/js/granim.js') }}"></script>
     <!-- Styles -->
     <style>
     body {
@@ -37,16 +37,49 @@
         padding: 0;
         margin: 0;
         height: 100vh;
-        background-color: #263238;
+        /*background-color: #263238;*/
+    }
+    #canvas-basic {
+        position: absolute;
+        display: block;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+    .content{
+        z-index: 99;
     }
 </style>
 </head>
 <body>
     <div class="center">
+        <canvas id="canvas-basic"></canvas>
         <div class="content">
             <h1>Intropis</h1>
             <p>By Ibnu Abdul Azis</p>
         </div>
+        <canvas id="canvas-basic"></canvas>
     </div>
 </body>
+<script>
+    var granimInstance = new Granim({
+        element: '#canvas-basic',
+        name: 'basic-gradient',
+        direction: 'left-right',    // 'diagonal', 'top-bottom', 'radial'
+        opacity: [1, 1],
+        isPausedWhenNotInView: true,
+        states : {
+            "default-state": {
+                gradients: [
+                ['#AA076B', '#61045F'],
+                ['#02AAB0', '#00CDAC'],
+                ['#DA22FF', '#9733EE']
+                ]
+            }
+        }
+    });
+</script>
 </html>
